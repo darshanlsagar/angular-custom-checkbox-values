a5eb805d04b2e93f6311232d17f9fc423c4752d4
darshanlsagar <darshanlsagar@gmail.com>
2020-03-05 18:32:15 +0530

    Test cases updation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,21 @@ declare var window: any;
 })
 export class AppComponent {
   title = 'directives';
-  v= new FormControl();
+  fcl1 = new FormControl();
+  fcl2 = new FormControl();
+  cb2: any = "Y";
+  fcp = new FormControl();
 
   constructor(){
 	window.x = this;
+  }
+
+  toggleCB(){
+	//   let this.cb2 = this.this.cb22;
+	  if(this.cb2){
+		this.cb2 = false;
+	  } else {
+		  this.cb2 = Math.random();
+	  }
   }
 }
